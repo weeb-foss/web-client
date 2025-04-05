1d23844e20bac8cde257a01d924d78d4a18e3825
simxnet <simxnet@envs.net>
2025-04-06 01:38:23 +0200

    chore: startup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Orbix Web Client
+This repository is being worked on at the moment, join [our Discord server](https://discord.gg/qhuQkGWmsE) for further information on future updates.
---
+++ src/routes/__layout.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode, JSX } from "react";
+import { TuonoScripts } from "tuono";
+
+import "../styles/global.css";
+
+interface RootLayoutProps {
+	children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
+	return (
+		<html lang="en" dir="ltr">
+			<body>
+				<main>{children}</main>
+				<TuonoScripts />
+			</body>
+		</html>
+	);
+}
